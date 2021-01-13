96d4d9c819d8f83452fe77a65a4d1c303b2cfd5b
Guilherme Fontanella <gui.jmf@hotmail.com>
2021-01-13 15:51:20 -0300

    CRUD angular

---
+++ frontend/src/app/components/template/product/product-update/product-update.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from './../product.service';
+import { Product } from './../product.model';
+
+@Component({
+  selector: 'app-product-update',
+  templateUrl: './product-update.component.html',
+  styleUrls: ['./product-update.component.css']
+})
+export class ProductUpdateComponent implements OnInit {
+
+  product: Product;
+
+  constructor(private productService: ProductService, private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    const id = +this.route.snapshot.paramMap.get('id')
+    this.productService.readById(id).subscribe(product => {
+      this.product = product;
+    });
+  }
+
+  updateProduct(): void {
+    this.productService.update(this.product).subscribe(() => {
+      this.productService.showMessage("Produto alterado com sucesso!")
+      this.router.navigate(['/product'])
+    })
+  }
+
+
+  cancel(): void {
+    this.router.navigate(['/product'])
+  }
+
+}
